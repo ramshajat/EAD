efcbe41a763406fca384a56265b845bb5ffc357a
Ramsha Jat <ramsha.bscsf19@iba-suk.edu.pk>
2023-05-16 20:25:44 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Button varient='primary'>hello</Button>
+      <form>
+        Name:<input type='text' required autoFocus=" "></input>
+        <br>
+        </br>
+        gender:<input type='radio' value='m' checked></input>Male
+        <br></br>
+        cb:<input type='checkbox' value='m' checked></input>Male
+        <input type='submit'></input>
+      </form>
+      <fieldset>
+        <legend>clander</legend>
+        Date:<input type='date' name='date'/>
+        <br></br>
+        Date and Time:<input type='datetime-local' name='date'/>
+        <br></br>
+        Time:<input type='time' name='date'/>
+        <br></br>
+        Month :<input type='Month' name='date'/>
+        <br></br>
+        week:<input type='week' name='date'/>
+        <br></br>
+        number:<input type='number' name='date' min ='10'/>
+        Range:<input type='Range' name='date' min ='10'/>
+        <br></br>
+      </fieldset>
+    </div>
+  );
+}
+
+export default App;
